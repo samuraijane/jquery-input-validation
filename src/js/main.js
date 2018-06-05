f990b1e956815cf7ed87d17ac125d76692a08590
Matthew Day <mday1@central1.com>
2018-06-04 23:34:03 -0400

    Add footnote for email validation

--- src/js/main.js
+++ src/js/main.js
@@ -291,7 +291,7 @@
 			app.handleReset();
     },
 
-    isValidEmail: function(email) {
+    isValidEmail: function(email) {  //c005
     	let _promise = new Promise((res, rej) => {
         let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         res(re.test(email));
